50548958a58b47d402e08f985a2e3dcc758edfe3
Lenny4 <alexandre.beaujour@hotmail.fr>
2017-11-10 07:07:06 +0100

    fix modal display bug

--- wp-content/themes/legion/sidebar.php
+++ wp-content/themes/legion/sidebar.php
@@ -31,7 +31,6 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Change Language</h4>
                 </div>
                 <div class="modal-body">
                     <div id="qtransSelector"
